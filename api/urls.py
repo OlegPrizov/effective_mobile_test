bd8ba7ca798e01d4304f079b63cb9e10e5942a7c
Олег Призов <oleg12345prisov@gmail.com>
2025-01-31 22:37:53 +0300

    docs added

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import OrderViewSet
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 router = DefaultRouter()
 router.register(r'orders', OrderViewSet)
@@ -11,4 +12,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='api:schema'), name='swagger-ui'),
 ]
